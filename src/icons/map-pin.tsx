88c571e29eec37c3d74e9afdcb328eb51540e04e
Damokless <damokless@protonmail.com>
2024-07-02 13:42:20 +0200

    feat : new icons

---
+++ src/icons/map-pin.tsx
@@ -0,0 +1,20 @@
+export default function MapPin() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			className="icon icon-tabler icon-tabler-map-pin h-full"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			strokeWidth="1.5"
+			stroke="#a1a1aa"
+			fill="none"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M0 0h24v24H0z" stroke="none" />
+			<path d="M9 11a3 3 0 1 0 6 0 3 3 0 0 0-6 0" />
+			<path d="M17.657 16.657 13.414 20.9a2 2 0 0 1-2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z" />
+		</svg>
+	);
+}
---
+++ src/icons/sunset.tsx
@@ -0,0 +1,21 @@
+export default function SunSet() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			className="icon icon-tabler icon-tabler-sunset"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+			strokeWidth="1.5"
+			stroke="#a1a1aa"
+			fill="none"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M3 17h1m16 0h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7m-9.7 5.7a4 4 0 0 1 8 0" />
+			<path d="M3 21l18 0" />
+			<path d="M12 3v6l3 -3m-6 0l3 3" />
+		</svg>
+	);
+}
