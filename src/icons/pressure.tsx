d27aabb13f229aaf02e5b989ce97560f8bf63be7
damokless <damokless@protonmail.com>
2024-07-06 18:58:02 +0200

    feat : new icon

---
+++ src/icons/pressure.tsx
@@ -0,0 +1,22 @@
+export default function Pressure() {
+	return(
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			className="icon icon-tabler icon-tabler-arrows-down"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+			strokeWidth="1.5"
+			stroke="#a1a1aa"
+			fill="none"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M7 21l0 -18" />
+			<path d="M20 18l-3 3l-3 -3" />
+			<path d="M4 18l3 3l3 -3" />
+			<path d="M17 21l0 -18" />
+		</svg>
+	)
+}
